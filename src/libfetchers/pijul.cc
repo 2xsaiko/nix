8427170521f769270e71029afeb6b99d39dff7df
Marco Rebhan <me@dblsaiko.net>
2023-11-23 01:23:53 +0100

    libfetchers/pijul: Style pass

--- src/libfetchers/pijul.cc
+++ src/libfetchers/pijul.cc
@@ -1,76 +1,94 @@
-// #include "cache.hh"
 #include "fetchers.hh"
 #include "store-api.hh"
 
-// #include "fetch-settings.hh"
-
-#include <string.h>
-
 using namespace std::string_literals;
+using namespace std::string_view_literals;
 
 namespace nix::fetchers {
 
-struct PijulInputScheme : InputScheme {
-  std::optional<Input> inputFromURL(const ParsedURL &url) const override {
-    if (url.scheme != "pijul+http" && url.scheme != "pijul+https")
-      return {};
+struct PijulInputScheme : InputScheme
+{
+    [[nodiscard]]
+    std::optional<Input> inputFromURL(const ParsedURL &url) const override
+    {
+        if (url.scheme != "pijul+http" && url.scheme != "pijul+https") {
+            return {};
+        }
 
-    auto url2(url);
-    url2.scheme = std::string(url2.scheme, 6);
-    url2.query.clear();
+        auto url2(url);
+        url2.scheme = std::string(url2.scheme, 6);
+        url2.query.clear();
 
-    Attrs attrs;
-    attrs.emplace("type", "pijul");
-    attrs.emplace("url", url2.to_string());
+        Attrs attrs;
+        attrs.emplace("type"s, "pijul"s);
+        attrs.emplace("url"s, url2.to_string());
 
-    return inputFromAttrs(attrs);
-  }
+        return inputFromAttrs(attrs);
+    }
 
-  std::optional<Input> inputFromAttrs(const Attrs &attrs) const override {
-    if (maybeGetStrAttr(attrs, "type") != "pijul")
-      return {};
+    [[nodiscard]]
+    std::optional<Input> inputFromAttrs(const Attrs &attrs) const override
+    {
+        if (maybeGetStrAttr(attrs, "type") != "pijul") {
+            return {};
+        }
 
-    for (auto &[name, _] : attrs)
-      if (name != "type" && name != "url")
-        throw Error("unsupported Pijul input attribute '%s'", name);
+        for (const auto &[name, _]: attrs) {
+            if (name != "type" && name != "url") {
+                throw Error("unsupported Pijul input attribute '%s'", name);
+            }
+        }
 
-    parseURL(getStrAttr(attrs, "url"));
+        parseURL(getStrAttr(attrs, "url"));
 
-    Input input;
-    input.attrs = attrs;
-    return input;
-  }
+        Input input;
+        input.attrs = attrs;
+        return input;
+    }
 
-  bool hasAllInfo(const Input &input) const override { return true; }
+    [[nodiscard]]
+    bool hasAllInfo(const Input &input) const override
+    {
+        return true;
+    }
 
-  ParsedURL toURL(const Input &input) const override {
-    auto url = parseURL(getStrAttr(input.attrs, "url"));
-    if (url.scheme != "pijul")
-      url.scheme = "pijul+" + url.scheme;
-    return url;
-  }
+    [[nodiscard]]
+    ParsedURL toURL(const Input &input) const override
+    {
+        auto url = parseURL(getStrAttr(input.attrs, "url"));
 
-  std::pair<StorePath, Input> fetch(ref<Store> store,
-                                    const Input &_input) override {
-    Input input(_input);
+        if (url.scheme != "pijul") {
+            url.scheme = "pijul+"s + url.scheme;
+        }
 
-    Path tmpDir = createTempDir();
-    AutoDelete delTmpDir(tmpDir, true);
-    auto repoDir = tmpDir + "/source";
+        return url;
+    }
 
-    auto url = parseURL(getStrAttr(input.attrs, "url"));
-    auto repoUrl = url.base;
+    std::pair<StorePath, Input> fetch(
+        ref<Store> store,
+        const Input &_input
+    ) override
+    {
+        Input input(_input);
 
-    runProgram("pijul", true, {"clone", repoUrl, repoDir}, {}, true);
-    deletePath(repoDir + "/.pijul");
+        const Path tmpDir = createTempDir();
+        const AutoDelete delTmpDir(tmpDir, true);
+        const auto repoDir = tmpDir + "/source"sv;
 
-    auto storePath = store->addToStore(input.getName(), repoDir);
+        const auto url = parseURL(getStrAttr(input.attrs, "url"));
+        const auto &repoUrl = url.base;
 
-    return {std::move(storePath), input};
-  }
+        runProgram("pijul"s, true, { "clone"s, repoUrl, repoDir }, {}, true);
+        deletePath(repoDir + "/.pijul"sv);
+
+        auto storePath = store->addToStore(input.getName(), repoDir);
+
+        return { std::move(storePath), input };
+    }
 };
 
-static auto rPijulInputScheme = OnStartup(
-    [] { registerInputScheme(std::make_unique<PijulInputScheme>()); });
+static auto rPijulInputScheme = OnStartup([] {
+    registerInputScheme(std::make_unique<PijulInputScheme>());
+});
 
 } // namespace nix::fetchers
